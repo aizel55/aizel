0ecdd496c5ca7f0a9a4811a835d0d329ba270a1b
aizel55 <of.frozen.whale@gmail.com>
2017-03-25 10:31:41 +0900

    Add files via upload

---
+++ web/callback.php
@@ -0,0 +1,98 @@
+<?php
+// アクセスキー
+$gnaviaccesskey = getenv('gnavi_access_key');
+$accessToken = getenv('LINE_CHANNEL_ACCESS_TOKEN');
+
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$jsonObj = json_decode($json_string);
+
+$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
+//メッセージ取得
+$text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+//ReplyToken取得
+$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+
+//緯度取得
+$latitude = $jsonObj->{"events"}[0]->{"message"}->{"latitude"};
+//経度取得
+$longitude = $jsonObj->{"events"}[0]->{"message"}->{"longitude"};
+
+
+//エンドポイントのURIとフォーマットパラメータを変数に入れる
+$uri   = "http://api.gnavi.co.jp/RestSearchAPI/20150630/";
+//APIアクセスキーを変数に入れる
+$acckey= $gnaviaccesskey;
+//返却値のフォーマットを変数に入れる
+$format= "json";
+//緯度・経度、範囲を変数に入れる
+
+// 業態がラーメン屋さんを意味するぐるなびのコード(大業態マスタ取得APIをコールして調査)
+$category_s = "RSFST08008";
+
+$hit_per_page = "5";
+
+//緯度経度は日本測地系で日比谷シャンテのもの。範囲はrange=1で300m以内を指定している。
+$range = 1;
+
+//URL組み立て
+$url  = sprintf("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s", $uri, "?format=", $format, "&keyid=", $acckey, "&latitude=", $latitude,"&longitude=",$longitude,"&category_s=",$category_s,"&range=",$range,"&hit_per_page=",$hit_per_page);
+//API実行
+$json = file_get_contents($url);
+//取得した結果をオブジェクト化
+$obj  = json_decode($json);
+
+$total_hit_count = $obj->{'total_hit_count'};
+$result = "";
+
+//結果をパース
+//トータルヒット件数、店舗番号、店舗名、最寄の路線、最寄の駅、最寄駅から店までの時間、店舗の小業態を出力
+if ($total_hit_count === null) {
+    $result .= "近くにラーメン屋さんはありません。";
+}else{
+    $result .= "近くにあるラーメン屋さんです。\n\n";
+
+    foreach((array)$obj as $key => $val){
+      if(strcmp($key, "rest") == 0){
+          foreach((array)$val as $restArray){
+               $result .= $restArray->{'name'}."\n";
+               $result .= $restArray->{'url'}."\n\n";
+              }
+     
+          }
+    }
+    $result.="Powered by ぐるなび";
+}
+// //文字列であるかをチェック
+// function checkString($input)
+// {
+ 
+//     if(isset($input) && is_string($input)) {
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+//返信データ作成
+$response_format_text = [
+	"type" => "text",
+	"text" => $result
+	];
+
+$post_data = [
+	"replyToken" => $replyToken,
+	"messages" => [$response_format_text]
+	];
+
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+    curl_setopt($ch, CURLOPT_POST,true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json; charser=UTF-8',
+      'Authorization: Bearer ' . $accessToken
+    ));
+    $result = curl_exec($ch);
+    curl_close($ch);
+
---
+++ web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>API連係テストです。</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+更新確認
+</body>
+</html>
