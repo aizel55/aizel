31c11bc622d4e1730971098557dac205448c67ca
Atsushi N <of.frozen.whale@gmail.com>
2017-03-25 05:18:09 +0000

    template use

--- web/callback.php
+++ web/callback.php
@@ -76,9 +76,9 @@ if ($total_hit_count === null) {
         "text" => "こんにちわ 何かご用ですか？",
         "actions" => [
             [
-              "type" => "postback",
-              "label" => "はい",
-              "data" => "action=rsv&itemid=111"
+                  "type" => "uri",
+                  "label" => "詳しく見る（ブラウザ起動）",
+                  "uri" => "http://www.yahoo.co.jp/"
             ]
         ]
     ]
