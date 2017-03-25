00060443da41fc58288c00443d929e766d838904
Atsushi N <of.frozen.whale@gmail.com>
2017-03-25 03:52:04 +0000

    link github

--- web/callback.php
+++ web/callback.php
@@ -57,7 +57,7 @@ if ($total_hit_count === null) {
           foreach((array)$val as $restArray){
                $result .= $restArray->{'name'}."\n";
                $result .= $restArray->{'url'}."\n";
-               $result .= $restArray->{'image_url'}->{'shop_image1'}."\n";
+               $result .= $restArray->{'shop_image1'}."\n";
 
               }
      
