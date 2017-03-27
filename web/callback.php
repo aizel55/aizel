66fd75f4bfaf13c8ee25dab7b657476123b645e7
Atsushi N <of.frozen.whale@gmail.com>
2017-03-27 03:44:23 +0000

    PR long trim

--- web/callback.php
+++ web/callback.php
@@ -92,7 +92,7 @@ if ($total_hit_count === null) {
               $get_name2 =$restArray->{"name"};
               $get_url2 =$restArray->{"url"};
               $get_pr2 =$restArray->{"pr"}->{"pr_long"};
-              if(empty($get_pr2)){
+              if(empty(trim($get_pr2))){
                 $get_pr2 ="PRテキストはありません。";
               }
               $get_prtext2　= mb_strimwidth($get_pr2, 0, 50, "...");
