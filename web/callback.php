fa41c25561045608b5a981e194723eb2283a45a3
Atsushi N <of.frozen.whale@gmail.com>
2017-03-27 03:56:11 +0000

    PR comment test

--- web/callback.php
+++ web/callback.php
@@ -92,10 +92,11 @@ if ($total_hit_count === null) {
               $get_name2 =$restArray->{"name"};
               $get_url2 =$restArray->{"url"};
               $get_pr2 =$restArray->{"pr"}->{"pr_long"};
-              //if(empty(trim($get_pr2))){
-              $get_pr2 ="PRテキストはありません。";
-              //}
-              $get_prtext2　= mb_strimwidth($get_pr2, 0, 50, "...");
+              if(empty(trim($get_pr2))){
+                $get_pr2 ="PRテキストはありません。";
+              }
+//              $get_prtext2　= mb_strimwidth($get_pr2, 0, 50, "...");
+              $get_prtext2　= $get_pr2;
 
           }
      
