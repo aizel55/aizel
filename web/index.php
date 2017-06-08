88aaa4701cfed656033ba587f470291b8c9acd22
Atsushi N <of.frozen.whale@gmail.com>
2017-06-08 02:32:07 +0000

    test

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>リファクタリング</title>
+<title>LINEリファクタリング</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  LINE BOT対応
+  LINE BOT対応(動作テスト)
 </body>
 </html>
